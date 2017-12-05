5387517b2363df4d41bf4d7687fe46ec7b51c40a
mythLabs <31563084+mythLabs@users.noreply.github.com>
2017-12-05 13:59:54 +0530

    sendfile to sendFile

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.static(path.join(__dirname,'dist')));
 
 app.get('*',(req,res)=>{
- res.sendfile(path.join(__dirname,'dist/index.html'));
+ res.sendFile(path.join(__dirname,'dist/index.html'));
 });
 
 const port =process.env.PORT || 3000;
